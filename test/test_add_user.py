cfa30aaf059ee4e6892076c79b160659d76d3d6b
Дарья <karazanova@bk.ru>
2025-04-30 14:52:53 +0300

    Удалены фрагменты с созданием фикстуры для созданных классов

--- test/test_add_user.py
+++ test/test_add_user.py
@@ -5,4 +5,3 @@ def test_add_user(app):
     app.session.login("admin", "secret")
     app.user.add_new(User("First", "User", "Home", "N.Novgorod", "112", "u.first@mail.ru", "1", "2001", "May"))
     app.session.logout()
-
