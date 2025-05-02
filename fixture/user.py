db7bc00e69ef145b6ee4b98009e0147a9e5b1965
Дарья <karazanova@bk.ru>
2025-05-02 10:56:23 +0300

    1. Добавлен переход на главную страницу перед созданием/редактированием контакта 2. Заполнение полей контакта выделено в отдельную функцию

--- fixture/user.py
+++ fixture/user.py
@@ -6,37 +6,28 @@ class UserHelper:
     def add_new(self, user):
         wd = self.app.wd
         wd.find_element_by_link_text("add new").click()
-        wd.find_element_by_name("firstname").click()
-        wd.find_element_by_name("firstname").send_keys(user.firstname)
-        wd.find_element_by_name("lastname").click()
-        wd.find_element_by_name("lastname").send_keys(user.lastname)
-        wd.find_element_by_name("company").click()
-        wd.find_element_by_name("company").send_keys(user.company)
-        wd.find_element_by_name("address").click()
-        wd.find_element_by_name("address").send_keys(user.address)
-        wd.find_element_by_name("home").click()
-        wd.find_element_by_name("home").send_keys(user.home)
-        wd.find_element_by_name("email").click()
-        wd.find_element_by_name("email").send_keys(user.email)
-        wd.find_element_by_name("bday").click()
-        wd.find_element_by_name("bday").send_keys(user.bday)
-        wd.find_element_by_name("bmonth").click()
-        wd.find_element_by_name("bmonth").send_keys(user.bmonth)
-        wd.find_element_by_name("byear").click()
-        wd.find_element_by_name("byear").send_keys(user.byear)
+        self.user_fields(user)
         wd.find_element_by_xpath("//div[@id='content']/form/input[20]").click()
-        wd.find_element_by_link_text("home").click()
-
-    def delete_first_user(self):
-        wd = self.app.wd
-        wd.find_element_by_name("selected[]").click()
-        wd.find_element_by_xpath("//input[@value='Delete']").click()
         wd.find_element_by_link_text("home").click()
 
     def edit_first_user(self, user):
         wd = self.app.wd
+        self.app.open_home_page()
         wd.find_element_by_xpath("//img[@alt='Edit']").click()
         wd.find_element_by_xpath("//form[@action='edit.php']").click()
+        self.user_fields(user)
+        wd.find_element_by_xpath("//div[@id='content']/form/input[21]").click()
+        wd.find_element_by_link_text("home").click()
+
+    def delete_first_user(self):
+        wd = self.app.wd
+        self.app.open_home_page()
+        wd.find_element_by_name("selected[]").click()
+        wd.find_element_by_xpath("//input[@value='Delete']").click()
+        wd.find_element_by_link_text("home").click()
+
+    def user_fields(self, user):
+        wd = self.app.wd
         wd.find_element_by_name("firstname").click()
         wd.find_element_by_name("firstname").clear()
         wd.find_element_by_name("firstname").send_keys(user.firstname)
@@ -61,7 +52,6 @@ class UserHelper:
         wd.find_element_by_name("bmonth").send_keys(user.bmonth)
         wd.find_element_by_name("byear").click()
         wd.find_element_by_name("byear").send_keys(user.byear)
-        wd.find_element_by_xpath("//div[@id='content']/form/input[21]").click()
 
 
 
