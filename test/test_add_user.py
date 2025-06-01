f790ca764a60e480db51354de5dcfff8ce5c40cb
Дарья <karazanova@bk.ru>
2025-06-01 19:37:03 +0300

    Загрузка тестовых данных из файлов

--- test/test_add_user.py
+++ test/test_add_user.py
@@ -1,27 +1,9 @@
 # -*- coding: utf-8 -*-
 from model.user import User
-import pytest
-import random
-import string
 
-def random_string(prefix, maxlen):
-    symbols = string.ascii_letters + string.digits + " "*10
-    return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
 
-def random_phone(prefix, maxlen):
-    symbols = string.digits + " "*3
-    return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
-
-def random_email(prefix, maxlen):
-    symbols = string.ascii_letters + string.digits + " "
-    return "".join([random.choice(symbols) for i in range(random.randrange(maxlen))]) + prefix
-
-testdata = [User(firstname=random_string("User", 7), lastname=random_string("lastname", 7), address=random_string("", 10), email=random_email("@mail.ru", 10), homephone=random_phone("+7", 10))
-    for i in range(5)
-]
-
-@pytest.mark.parametrize("user", testdata, ids=(repr(x) for x in testdata))
-def test_add_user(app, user):
+def test_add_user(app, json_users):
+    user = json_users
     old_users = app.user.get_users_list()
     app.user.add_new(user)
     assert len(old_users) + 1 == app.user.count()
