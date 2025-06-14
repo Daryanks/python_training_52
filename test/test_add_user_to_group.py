9e827ad3dff7575fdfb4efc2043c54480817af8d
Дарья <karazanova@bk.ru>
2025-06-14 11:27:45 +0300

    Тесты для добавления контакта в группу и удаления контакта из группы

---
+++ test/test_add_user_to_group.py
@@ -0,0 +1,18 @@
+from fixture.orm import ORMFixture
+from model.group import Group
+from model.user import User
+import random
+
+db = ORMFixture(host="127.0.0.1", name="addressbook", user="root", password="")
+
+def test_group_list(app):
+    group = Group(id="37", name="Test")
+    users = db.get_users_not_in_group(group)
+    users_in_group = db.get_users_in_group(group)
+    user = random.choice(users)
+    user_id = user.id
+    app.user.add_user_to_group(user_id, group)
+    new_users_in_group = db.get_users_in_group(group)
+    users_in_group.append(user)
+    assert sorted(users_in_group, key=User.id_or_max) == sorted(new_users_in_group, key=User.id_or_max)
+
---
+++ test/test_delete_user_from_group.py
@@ -0,0 +1,21 @@
+from fixture.orm import ORMFixture
+from model.group import Group
+from model.user import User
+import random
+
+db = ORMFixture(host="127.0.0.1", name="addressbook", user="root", password="")
+
+def test_delete_user_from_group(app):
+    group = Group(id="37", name="Test")
+    if len(db.get_users_in_group(group)) == 0:
+        users_not_in_group = db.get_users_not_in_group(group)
+        user = random.choice(users_not_in_group)
+        user_id = user.id
+        app.user.add_user_to_group(user_id, group)
+    users_in_group = db.get_users_in_group(group)
+    user_g = random.choice(users_in_group)
+    user_id = user_g.id
+    app.user.delete_user_from_group(user_id, group)
+    new_users_in_group = db.get_users_in_group(group)
+    users_in_group.remove(user_g)
+    assert sorted(users_in_group, key=User.id_or_max) == sorted(new_users_in_group, key=User.id_or_max)
