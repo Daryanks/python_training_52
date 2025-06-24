db539be7a34949f708ddc01d980fe06783cfab63
Дарья <karazanova@bk.ru>
2025-06-24 14:06:35 +0300

    Fixed

--- test/test_add_user.py
+++ test/test_add_user.py
@@ -4,10 +4,10 @@ from model.user import User
 
 def test_add_user(app, db, json_users, check_ui):
     user = json_users
-    old_users = db.get_user_list()
+    old_users = db.get_users_list()
     app.user.add_new(user)
     assert len(old_users) + 1 == app.user.count()
-    new_users = db.get_user_list()
+    new_users = db.get_users_list()
     old_users.append(user)
     assert sorted(old_users, key=User.id_or_max) == sorted(new_users, key=User.id_or_max)
     if check_ui:
